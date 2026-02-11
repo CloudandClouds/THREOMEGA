fb1f766dbe9f99abe73ce477d7fb97cb8de4be97
DHANUSHVIJAY <sdhanushvijay50@gmail.com>
2026-02-11 14:09:11 +0530

    `Added import statement and updated export in Home.jsx`

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,6 +14,7 @@ import Scoreboard from '../components/Home/Scoreboard';
 import ProgramInfo from '../components/Home/ProgramInfo';
 
 
+
 class HomePage extends Component {
   render() {
     return (
